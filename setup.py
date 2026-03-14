f0321869387752347eca7c390e753fe4d5fa2203
Suraj <surajgaikwad345@gmail.com>
2026-03-14 18:12:15 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="AI-Medical-Chatbot",
+    version="0.1.0",
+    author="Suraj Gaikwad",
+    author_email="surajgaikwad345@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
